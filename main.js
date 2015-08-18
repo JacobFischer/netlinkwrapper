6ffaebb1082ee4f86c140655686054258caba8d6
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-08-18 15:22:15 -0500

    Able to require like any other module

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var netlinksocket = require('bindings')('netlinksocket');
-
-var netlink = new netlinksocket.NetLinkWrapper();
-
-netlink.connect("localhost", 3000);
-
-netlink.send("Am I people? Or am I dancer?");
-
-while(true) {
-    var str = netlink.read(1024);
-    console.log("reading... ", str);
-}
+module.exports = require('bindings')('netlinksocket');
